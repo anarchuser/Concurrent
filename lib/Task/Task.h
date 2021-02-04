379e821abe2c64836b9f120881b6fb239c5f20ee
Aaron Alef <aaron.alef@code.berlin>
2021-02-04 12:08:16 +0100

    feat(task): merge Task and TimedTask and ensure it working with void

--- lib/Task/Task.h
+++ lib/Task/Task.h
@@ -1,46 +1,148 @@
 #ifndef CONCURRENT_TASK_H
 #define CONCURRENT_TASK_H
 
+#include "../../config.h"
+
 #include "ITask.h"
 
 #include "../Future/Future.h"
+#include "../helper.h"
 
 #include <atomic>
+#include <chrono>
 #include <functional>
+#include <thread>
+#include <type_traits>
 
-
+/** Task returning a value */
 template <typename R>
 class Task: public ITask {
 public:
-    explicit Task (std::function <R()> && task): task {std::forward <std::function <R()>> (task)} {}
+    explicit Task (std::function <R()> && task):
+            task {std::forward <std::function <R()>> (task)},
+            ctor {std::chrono::high_resolution_clock::now()} {}
     Task (Task const & other) = delete;
-    Task (Task && other) { * this = std::move (other); }
+    Task (Task && other) noexcept { * this = std::move (other); }
+    ~Task () {
+        if (!done) return;
+        dtor = std::chrono::high_resolution_clock::now();
+        accumulated_idle += (start - ctor).count();
+        accumulated_work += (end  - start).count();
+        STD_OSTREAM <<
+                "Task ID "    << ID <<
+                "\tPointer " << this <<
+                "\tThread #" << std::this_thread::get_id() <<
+                "\tctor "    << ctor     <<
+                "\tstart "   << start    <<
+                "\tend "     << end      <<
+                "\tdtor "    << dtor     <<
+                "\tidle "    << (start - ctor)  <<
+                "\twork "    << (end   - start) <<
+                "\tlinger "  << (dtor  - end)   <<
+                "\ttotal "   << (dtor  - ctor) << std::endl;
+    }
 
     Task & operator = (Task && other) {
         task = std::move (other.task);
+        ctor  = other.ctor;
+        start = other.start;
+        end   = other.end;
+        dtor  = other.dtor;
         result = other.result;
         done = other.done;
         return * this;
     }
+
     std::shared_ptr <R> operator () () {
         if (run) return await();
         run = true;
+        start = std::chrono::high_resolution_clock::now();
         * result = task();
         * done = true;
+        end = std::chrono::high_resolution_clock::now();
         return result;
     }
     std::shared_ptr <R> await() const {
         while (!isDone()) std::this_thread::yield();
         return result;
     }
-    Future <R> getResult() const {
-        return Future <R> (result, done);
-    }
+
+    static std::atomic <std::size_t> accumulated_idle;
+    static std::atomic <std::size_t> accumulated_work;
 
 private:
     std::function <R()> task;
     std::shared_ptr <R> result = std::make_shared <R>();
+
+    std::chrono::time_point <std::chrono::high_resolution_clock> ctor, start, end, dtor;
 };
+
+/** Task without return value */
+template <>
+class Task <void>: public ITask {
+public:
+    explicit Task (std::function <void()> && task):
+            task {std::forward <std::function <void()>> (task)},
+            ctor {std::chrono::high_resolution_clock::now()} {}
+    Task (Task const & other) = delete;
+    Task (Task && other) noexcept { * this = std::move (other); }
+    ~Task () {
+        if (end <= start) return;
+        dtor = std::chrono::high_resolution_clock::now();
+        accumulated_idle += (start - ctor).count();
+        accumulated_work += (end  - start).count();
+        STD_OSTREAM <<
+                    "Task ID "    << ID <<
+                    "\tPointer " << this <<
+                    "\tThread #" << std::this_thread::get_id() <<
+                    "\tctor "    << ctor     <<
+                    "\tstart "   << start    <<
+                    "\tend "     << end      <<
+                    "\tdtor "    << dtor     <<
+                    "\tidle "    << (start - ctor)  <<
+                    "\twork "    << (end   - start) <<
+                    "\tlinger "  << (dtor  - end)   <<
+                    "\ttotal "   << (dtor  - ctor) << std::endl;
+    }
+
+    Task & operator = (Task && other)  noexcept {
+        task = std::move (other.task);
+        ctor  = other.ctor;
+        start = other.start;
+        end   = other.end;
+        dtor  = other.dtor;
+        done = other.done;
+        return * this;
+    }
+
+    void operator () () {
+        if (run) return await();
+        run = true;
+        start = std::chrono::high_resolution_clock::now();
+        task();
+        * done = true;
+        end = std::chrono::high_resolution_clock::now();
+    }
+    void await() const {
+        while (!isDone()) std::this_thread::yield();
+    }
+
+    static std::atomic <std::size_t> accumulated_idle;
+    static std::atomic <std::size_t> accumulated_work;
+
+private:
+    std::function <void()> task;
+
+    std::chrono::time_point <std::chrono::high_resolution_clock> ctor, start, end, dtor;
+};
+
+template <typename R>
+std::atomic <std::size_t> Task <R>::accumulated_idle = 0;
+template <typename R>
+std::atomic <std::size_t> Task <R>::accumulated_work = 0;
+
+std::atomic <std::size_t> Task <void>::accumulated_idle = 0;
+std::atomic <std::size_t> Task <void>::accumulated_work = 0;
 
 #endif //CONCURRENT_TASK_H
 
--- test/lib/executor.cpp
+++ test/lib/executor.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch.hpp>
 
 #include "../../lib/Executor/Executor.h"
-#include "../../lib/Task/TimedTask.h"
+#include "../../lib/Task/Task.h"
 
 #include <iostream>
 #include <vector>
@@ -10,13 +10,13 @@ using namespace std;
 
 SCENARIO ("Executor basic I/O") {
     GIVEN ("A String executor and a list of strings") {
-        Executor <TimedTask> executor;
+        Executor <Task <void>> executor;
 
         REQUIRE (executor.empty());
         WHEN ("I insert the strings into the executor") {
-            CHECK_NOTHROW (executor.schedule (std::make_unique <TimedTask> ([] { LOG (INFO) << "task 1/3 successful"; })));
-            CHECK_NOTHROW (executor.schedule (std::make_unique <TimedTask> ([] { LOG (INFO) << "task 2/3 successful"; })));
-            CHECK_NOTHROW (executor.schedule (std::make_unique <TimedTask> ([] { LOG (INFO) << "task 3/3 successful"; })));
+            CHECK_NOTHROW (executor.schedule (Task <void> ([] { LOG (INFO) << "task 1/3 successful"; })));
+            CHECK_NOTHROW (executor.schedule (Task <void> ([] { LOG (INFO) << "task 2/3 successful"; })));
+            CHECK_NOTHROW (executor.schedule (Task <void> ([] { LOG (INFO) << "task 3/3 successful"; })));
         }
         CHECK_NOTHROW (executor.await());
         REQUIRE (executor.empty());
